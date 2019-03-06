e52b1dbbc0bb05a8598230bc901777c0b71f16e1
tpavankumar <tpavankumar@CSCINDAE741498.asiapac.globalcsc.net>
2019-03-07 00:28:53 +0530

    Second commit

---
+++ Fabonci/src/com/test/Sort.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.util.Arrays;
+
+public class Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[] = {25,100,50,45,6,2};
+		
+		Arrays.sort(a);
+		
+		System.out.printf("Modified a[] : %s", 
+                Arrays.toString(a));
+	}
+
+}
